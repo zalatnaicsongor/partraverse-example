efd0b7d48d0c02f503a4915f40ed29c5ea15de29
Csongor Zalatnai <csongor@zalatnai.hu>
2024-09-27 10:53:56 +0200

    further improvements

--- build.sbt
+++ build.sbt
@@ -1,15 +1,16 @@
-val scala3Version = "3.5.1"
+val scala3Version = "3.3.3"
 
 lazy val root = project
   .in(file("."))
   .settings(
     name := "partraverse-example",
     version := "0.1.0-SNAPSHOT",
-
     scalaVersion := scala3Version,
-
-    libraryDependencies += "org.scalameta" %% "munit" % "1.0.0" % Test
+    libraryDependencies += "org.scalameta" %% "munit" % "1.0.2" % Test,
+    libraryDependencies += "org.typelevel" %% "cats-effect" % "3.5.4"
   )
+
+scalacOptions += "-Wnonunit-statement"
 
 addCommandAlias("scalafmtFormatAll", "; scalafmtAll ; scalafmtSbt")
 addCommandAlias("lint", "; scalafmtAll ; scalafmtSbt ; scalafixAll")
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,34 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import cats.effect.ExitCode
+import cats.effect.IO
+import cats.effect.IOApp
+import cats.syntax.parallel.*
+import cats.syntax.traverse.*
 
-def msg = "I was compiled by Scala 3. :)"
+import java.util.concurrent.TimeUnit
+import scala.concurrent.blocking
+import scala.concurrent.duration.Duration
+import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object Main extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    val queue = Queue()
+
+    // emulate a background processing thread
+    System.out.println(Future {
+      blocking {
+        queue.tailQueue()
+      }
+    })
+    for {
+      _ <- (1 to 100).toList.traverse(elem => queue.offer("traverse " + elem))
+      _ <- IO.sleep(Duration.apply(2, TimeUnit.SECONDS))
+      _ <- (1 to 100).toList.parTraverse(elem => queue.offer("parTraverse " + elem))
+      _ <- IO.sleep(Duration.apply(2, TimeUnit.SECONDS))
+      _ <- (1 to 100).toList.parUnorderedTraverse(elem => queue.offer("parUnorderedTraverse " + elem))
+      _ <- IO.sleep(Duration.apply(2, TimeUnit.SECONDS))
+    } yield ExitCode.Success
+
+  }
+}
